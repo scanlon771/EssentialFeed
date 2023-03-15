7b432afecb0c7c591804f8af37637dd1c3974c07
Kenneth Scanlon <scanlon771@gmail.com>
2023-03-14 20:43:58 -0500

    Create EssentialFeed framework.

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Kenneth Scanlon on 3/14/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
